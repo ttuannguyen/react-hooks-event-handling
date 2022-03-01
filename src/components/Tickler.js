f66741317ab87986572bcb1a99ef418f65f59ca2
Tuan Nguyen <trentontuannguyen@gmail.com>
2022-02-28 21:27:44 -0500

    commit tree f0f5336f9b5b957254e0a14e9f8e835707b03aba

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 function Tickler() {
-  function tickle() {
-    console.log("Teehee!");
+  function tickle(event) {
+    console.log(event)
   }
+  //alternatively, use arrow syntax to write function inline
+  //return <button onClick={() => console.log("Teehee!")}>Tickle me!</button>
 
-  return <button>Tickle me!</button>;
+  //passing a function reference
+  return <button onClick={tickle}>Tickle me!</button>;
+
 }
 
 export default Tickler;
